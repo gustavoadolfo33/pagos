f5dfbc2fdc29437ef279199592b885ed98e749e7
gustavoadolfo33 <gustof123@hotmail.com>
2023-02-25 07:31:54 -0400

    segunda

---
+++ app/Controladores/ControladorUsuarios.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Parzibyte\Controladores;
+
+use Parzibyte\Modelos\ModeloUsuarios;
+use Parzibyte\Servicios\Comun;
+
+class ControladorUsuarios
+{
+
+    public static function index()
+    {
+        return view("usuarios");
+    }
+
+    public static function actualizarPalabraSecreta()
+    {
+        if (Comun::env("DENEGAR_USUARIOS", false)) {
+            return json(true);
+        }
+        $datos = json_decode(file_get_contents("php://input"));
+        return json(ModeloUsuarios::actualizarPalabraSecreta($datos->id, $datos->palabraSecretaActual));
+    }
+
+    public static function agregar()
+    {
+        if (Comun::env("DENEGAR_USUARIOS", false)) {
+            return json(true);
+        }
+
+        $datos = json_decode(file_get_contents("php://input"));
+        return json(ModeloUsuarios::agregar($datos->correo, $datos->palabraSecreta));
+    }
+
+    public static function eliminar($idUsuario)
+    {
+        if (Comun::env("DENEGAR_USUARIOS", false)) {
+            return json(true);
+        }
+
+        return json(ModeloUsuarios::eliminar($idUsuario));
+    }
+
+    public static function obtener()
+    {
+        return json(ModeloUsuarios::obtener());
+    }
+
+}
